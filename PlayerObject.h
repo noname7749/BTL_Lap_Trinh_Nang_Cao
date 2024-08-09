e2c8af37ff34c94ae99420348f3949e94de1e823
noname7749 <125619890+noname7749@users.noreply.github.com>
2024-08-09 19:30:03 +0700

    Update PlayerObject.h

--- PlayerObject.h
+++ PlayerObject.h
@@ -2,6 +2,7 @@
 #ifndef MAIN_OBJECT_H_
 #define MAIN_OBJECT_H_
 
+#include "stdafx.h"
 #include "CommonFunc.h"
 
 class PlayerObject : public BaseObject
@@ -10,19 +11,23 @@ public:
 	PlayerObject();
 	~PlayerObject();
 
-	bool LoadImg(std::string path, SDL_Renderer* screen);
+	bool LoadImg(string path, SDL_Renderer* screen);
 	void Show(SDL_Renderer* des);
-	void HandleInputAction(SDL_Event events, SDL_Renderer* screen);
+	void HandleInputAction(SDL_Event events, SDL_Renderer* screen, Mix_Chunk* flySound);
+
+	void Reset();
 
 	void DoFalling(SDL_Renderer* des);
 	bool GetFalling() const { return is_falling_; }
 	void SetFalling(bool falling) { is_falling_ = falling; }
 	void DoGround(SDL_Renderer* screen);
 	bool GetIsDie() const { return is_die_; }
+
+	Mix_Chunk* flySound;
 private:
 	bool is_falling_;
-	float x_val_;
-	float y_val_;
+	double x_val_;
+	double y_val_;
 	bool is_die_;
 };
 
