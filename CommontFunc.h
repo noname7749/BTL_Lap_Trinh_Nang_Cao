971404c11c0e592a8c9bee4d58cc2bd1f366354c
noname7749 <125619890+noname7749@users.noreply.github.com>
2024-08-09 19:26:35 +0700

    Update CommontFunc.h

--- CommontFunc.h
+++ CommontFunc.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_FUNC_H_
 #define COMMON_FUNC_H_
 
+#include "stdafx.h"
 #include <windows.h>
 #include <vector>
 #include <string>
@@ -11,23 +12,35 @@
 #include "TextObject.h"
 #include "BaseObject.h"
 
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 640;
-const int FRAMES_PER_SECOND = 55;
-const int GROUND_MAP = 600;
+using namespace std;
+
+static  SDL_Window* g_window = NULL;
+static SDL_Renderer* g_screen = NULL;
+
+static SDL_Event g_event;
+
+#define GROUND_MAP 600
 
 const int COLOR_KEY_R = 180;
 const int COLOR_KEY_G = 180;
 const int COLOR_KEY_B = 180;
-const int RENDER_DRAW_COLOR = 0xFF;
 
-static SDL_Window* g_window = nullptr;
-static SDL_Renderer* g_screen = nullptr;
-static SDL_Event g_event;
+const int RENDER_DRAW_COLOR = 0XFF;
 
-struct GeometricFormat {
+const int FRAMES_PER_SECOND = 55;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 640;
+
+struct GeometricFormat
+{
+public:
     GeometricFormat(int left, int top, int width, int height)
-        : left_(left), top_(top), width_(width), height_(height) {}
+    {
+        left_ = left;
+        top_ = top;
+        width_ = width;
+        height_ = height;
+    }
 
     int left_;
     int top_;
@@ -35,24 +48,26 @@ struct GeometricFormat {
     int height_;
 };
 
-struct ColorData {
+struct ColorData
+{
+public:
     ColorData(Uint8 r, Uint8 g, Uint8 b)
-        : red_(r), green_(g), blue_(b) {}
-
+    {
+        red_ = r, green_ = g, blue_ = b;
+    }
     Uint8 red_;
     Uint8 green_;
     Uint8 blue_;
 };
 
-namespace SDLCommonFunc {
+namespace SDLCommonFunc
+{
     bool CheckCollision(const SDL_Rect& object1, const SDL_Rect& object2);
     int ShowMenu(SDL_Renderer* g_screen, TTF_Font* font,
-        const std::string& menu1,
-        const std::string& menu2,
-        const std::string& img_name);
+        const string& menu1,
+        const string& menu2,
+        const string& img_name);
     int MyRandom(int min, int max);
 }
 
-void ShowStartScreen(SDL_Renderer* g_screen, TTF_Font* font);
-
-#endif 
+#endif
